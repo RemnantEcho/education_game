abc7dd83642a2b16ace05a0b194474d74f337a53
RemnantEcho <jsly097@gmail.com>
2023-07-31 20:10:37 +0100

    fixed integration of about implementation with main branch

--- client/js/geography.js
+++ client/js/geography.js
@@ -1,30 +1,30 @@
-const subjectButton = document.querySelector('#subject-icon');
-const subjectDropdown = document.querySelector('#subject-dropdown');
+// const subjectButton = document.querySelector('#subject-icon');
+// const subjectDropdown = document.querySelector('#subject-dropdown');
 
-// const showDropdown = (e) => {
+// // const showDropdown = (e) => {
+// //     e.preventDefault();
+// //     subjectDropdown.classList.remove('hide');
+
+// // }
+
+// const toggleDropdown = (e) => {
 //     e.preventDefault();
-//     subjectDropdown.classList.remove('hide');
-
+//     if (subjectDropdown.classList.contains('hide')) {
+//         subjectDropdown.classList.remove('hide');
+//     }
+//     else {
+//         subjectDropdown.classList.add('hide');
+//     }
+    
 // }
 
-const toggleDropdown = (e) => {
-    e.preventDefault();
-    if (subjectDropdown.classList.contains('hide')) {
-        subjectDropdown.classList.remove('hide');
-    }
-    else {
-        subjectDropdown.classList.add('hide');
-    }
-    
-}
+// const hideDropdown = (e) => {
+//     e.preventDefault();
+//     subjectDropdown.classList.add('hide');
+// }
 
-const hideDropdown = (e) => {
-    e.preventDefault();
-    subjectDropdown.classList.add('hide');
-}
+// subjectButton.addEventListener('click', toggleDropdown);
+// // subjectDropdown.addEventListener('click', showDropdown);
 
-subjectButton.addEventListener('click', toggleDropdown);
-// subjectDropdown.addEventListener('click', showDropdown);
-
-// subjectButton.addEventListener('mouseleave', hideDropdown);
-subjectDropdown.addEventListener('mouseleave', hideDropdown);
+// // subjectButton.addEventListener('mouseleave', hideDropdown);
+// subjectDropdown.addEventListener('mouseleave', hideDropdown);
